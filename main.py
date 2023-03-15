5be61926167fbeae1589e7bb678c8318206dcb63
awashington1 <126625038+awashington25@users.noreply.github.com>
2023-03-15 22:14:50 +0000

    a31 done

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+   import random
+7
+8    n1 = random.randint(0,100)
+9    n2 = random.randint(0,100)
+10   n3 = random.randint(0,100)
+11   print (n1, n2, n3)
+12
+13   if n1 < n2 < n3:
+14        print ( f'{n1} is the smallest number')
+15            elif n2 < n1 < n3:
+16        print ( f'{n2} is the smallest number')
+17      else:
+18        print ( f'{n3} is the smallest number')
+19
+20 if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
